9898f2a767a32196df60d2bbee9bf7e7b632822d
sema <sema@vistomail.com>
2021-12-05 15:27:41 +0300

    Initial commit

--- pool/share.go
+++ pool/share.go
@@ -15,7 +15,7 @@ import (
 	bolt "github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/decred/dcrd/chaincfg"
-	"github.com/decred/dcrd/dcrutil"
+	"github.com/HcashOrg/hcd/hcutil"
 )
 
 var (
