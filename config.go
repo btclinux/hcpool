0d184951da64531fcacd6260407c0b6377afe254
sema <sema@vistomail.com>
2021-12-05 15:24:02 +0300

    Initial commit

--- config.go
+++ config.go
@@ -22,7 +22,7 @@ import (
 
 	"github.com/decred/dcrd/certgen"
 	"github.com/decred/dcrd/chaincfg"
-	"github.com/HcashOrg/hcd/dcrutil"
+	"github.com/HcashOrg/hcd/hcutil"
 	"github.com/decred/slog"
 
 	"github.com/btclinux/hcpool/pool"
