5180b2ac144d879a72027a3bc26e637aef0b0c65
sema <sema@vistomail.com>
2021-12-05 15:27:03 +0300

    Initial commit

--- pool/payment.go
+++ pool/payment.go
@@ -14,7 +14,7 @@ import (
 
 	bolt "github.com/coreos/bbolt"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/decred/dcrd/dcrutil"
+	"github.com/HcashOrg/hcd/hcutil"
 )
 
 // Payment represents an outstanding payment for a pool account.
